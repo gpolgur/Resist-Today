114a709e921dd8231705fc739184c0bb8f148d84
Gabby Polgur <gabby@Gakkos-MacBook-Air.local>
2018-07-17 12:06:38 -0400

    some changes

---
+++ script.js
@@ -0,0 +1,6 @@
+var email = prompt("Please enter your email if you would like to sign up.");
+
+if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "You may be notified at " + email + " when any updates occur";
+}
